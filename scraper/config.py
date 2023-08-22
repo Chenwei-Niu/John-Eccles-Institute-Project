c5e822572b7463e29ced75ba406bd5b7a08c3bdc
u7384629 <u7384629@anu.edu.au>
2023-08-22 10:22:17 +1000

    added db

---
+++ scraper/config.py
@@ -0,0 +1,48 @@
+SPIDER_NAME = "event-spider"
+EVENTS_URLS = {
+    "medical school anu": {
+        "domain": "https://medicalschool.anu.edu.au",
+        "url": "https://medicalschool.anu.edu.au/news-events/events",
+        "xpath": {
+            "event_list": '//div[@class="clearfix marginbottom clear"]',
+            "event_item": './div[1]/a/@href',
+            "event_info": {
+                "title": '//*[@id="page-title"]/text()',
+                "description": '//*[@id="block-system-main"]/div/div/div/div[1]/div/div[5]/div/div/div/div//text()',
+                "speaker": "",
+                "date": '//div[@class="panel-pane pane-entity-field pane-node-field-date-time brd-label marginbottom"]/div/div/div//text()',
+                "venue": '//div[@class="panel-pane pane-entity-field pane-node-field-location marginbottom"]//p/text()',
+            }
+        }
+    },
+    "JCSMR anu": {
+        "domain": "https://jcsmr.anu.edu.au",
+        "url": "https://jcsmr.anu.edu.au/news-events/events",
+        "xpath": {
+            "event_list": '//div[@class="clearfix marginbottom clear"]',
+            "event_item": './div[1]/a/@href',
+            "event_info": {
+                "title": '//*[@id="page-title"]/text()',
+                "description": '//*[@id="block-system-main"]/div/div/div/div[1]/div/div[5]/div/div/div/div//text()',
+                "speaker": "",
+                "date": '//div[@class="panel-pane pane-entity-field pane-node-field-date-time brd-label marginbottom"]/div/div/div//text()',
+                "venue": '//div[@class="panel-pane pane-entity-field pane-node-field-location marginbottom"]//p/text()',
+            }
+        }
+    },
+    "computing school anu": {
+        "domain": "https://comp.anu.edu.au",
+        "url": "https://comp.anu.edu.au/newsroom",
+        "xpath": {
+            "event_list": '//article[@class="card"]',
+            "event_item": './a/@href',
+            "event_info": {
+                "title": '//*[@id="main-content"]/header/h1/text()',
+                "description": '//*[@id="skip-toc-target"]//text()',
+                "speaker": "",
+                "date": '//*[@id="skip-toc-target"]/p[1]/text()',
+                "venue": '//*[@id="skip-toc-target"]/p[2]/text()',
+            }
+        }
+    }
+}
