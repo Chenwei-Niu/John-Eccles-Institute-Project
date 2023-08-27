243c30b35c0b3b8f3c51c9e2473fb33a6cbefdbc
u7384629 <u7384629@anu.edu.au>
2023-08-27 18:16:02 +1000

    integrated forntend

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,23 @@
 from fastapi import FastAPI
 from models import *
 from sqlalchemy.orm import sessionmaker
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 engine = db_connect()
 Session= sessionmaker(bind=engine)
 db = Session()
+
 @app.get("/")
 async def root():
     return {"message": "server is live"}
