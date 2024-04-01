33d82cf2bc0b63f6ba2ae623c9cf9a70c7944d47
Chenwei Niu <u7377070@anu.edu.au>
2024-04-02 06:22:40 +1100

    Solved the page flash issue Methods: separate Search and seminar display from page.js and update the css file

---
+++ frontend/app/Components/SearchAndSeminarsComponent.js
@@ -0,0 +1,67 @@
+
+import { useEffect, useState } from 'react';
+import styles from '../page.module.css';
+
+
+export default function SearchAndSeminarsComponent() {
+  const [data, setData] = useState(null);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+  
+  useEffect(() => {
+    fetch('http://127.0.0.1:8000/get-events',{
+      credentials: 'include' // This setting in crucial to add cookies to cross-domain request header
+    })
+      .then(response => response.json())
+      .then(json => setData(json))
+      .catch(error => console.error(error));
+  }, []);
+
+
+  const handleSearchInputChange = (event) => {
+    console.log(searchTerm)
+    setSearchTerm(event.target.value)
+    searchEvents(event.target.value)
+  }
+
+  const searchEvents = (keywords) => {
+    fetch('http://127.0.0.1:8000/search-events?' +  new URLSearchParams({
+        searchTerm: keywords,
+      }),{
+        credentials: 'include' // This setting in crucial to add cookies to cross-domain request header
+      })
+      .then(response => response.json())
+      .then(json => setSearchResult(json))
+      .catch(error => console.error(error));
+  }
+
+  const renderEvent = (item) => {
+    return (
+      <div key={item.id} className={styles.eventCard}>
+        <a href={`${item.url}`}>
+        <p className={styles.seminarTitle}>{`${item.title}`}</p>
+        <p>{`Date: ${item.date}`}</p>
+        <p>{`Venue: ${item.venue}`}</p>
+        <div className={styles.ellipsisDescriptionContainer}><p style={{fontWeight:650}}>{`Description:`}</p>{`${item.description}`}</div>
+        </a>
+      </div>
+    )
+  }
+
+  return(
+    <div className={styles.searchAndSeminarWrapper}>
+      {/* <from className={styles.form} onSubmit={searchEvents}> */}
+        <input className={styles.searchInput} 
+          type="text"  value={searchTerm} 
+          placeholder={"search"} 
+          onChange={handleSearchInputChange} />
+      {/* </from> */}
+      {searchTerm ? searchResult.map(event => renderEvent(event)): 
+        data ? data.map(event => renderEvent(event)) : 'loading...'
+      }
+
+    </div>
+    
+  )
+}
+
